d4aad84bbf39093c059ae54fd71249abe506f9d5
Allie Signet <allie@cat-girl.gay>
2021-07-13 02:15:43 -0300

    cors

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 from quart import Quart, request
 from db import ShortenDB
+from quart_cors import cors
 import tldextract
 import os
 
 safelist = os.environ["SAFELIST"].split(";")
 
 app = Quart(__name__)
+app = cors(app, allow_origin="*",allow_methods=["GET","POST"])
+
 db = ShortenDB(app,os.environ["DB_PATH"])
 
 @app.route("/<token>")
